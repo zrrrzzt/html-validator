a3b63ff7a87e9095e9b14c5372d77f5a04f252a4
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-21 19:12:37 +0200

    Adds test for opts.url or opts.data

--- test/testInputs.js
+++ test/testInputs.js
@@ -25,8 +25,7 @@ describe('Validator - inputs', function(){
 
   });
 
-  /*
-  it('Should throw if opts.url is not specified', function(done){
+  it('Should throw if opts.url or opts.data is not specified', function(done){
 
     var opts = {};
 
@@ -34,7 +33,7 @@ describe('Validator - inputs', function(){
       assert.throws(function(){
           if(err) throw err;
         }, function(err){
-          if((err instanceof Error) && /Missing required param: url/.test(err)){
+          if((err instanceof Error) && /Missing required params: url or data/.test(err)){
             return true
           }
         },
@@ -45,23 +44,4 @@ describe('Validator - inputs', function(){
 
   });
 
-  it('Should throw if opts.data is not specified', function(done){
-
-    var opts = {};
-
-    validator(opts, function(err, data){
-      assert.throws(function(){
-          if(err) throw err;
-        }, function(err){
-          if((err instanceof Error) && /Missing required param: data/.test(err)){
-            return true
-          }
-        },
-        "Unexpected error"
-      );
-      done();
-    });
-
-  });
-  */
 });
