7becfc2cbaa91c7e2918af63e8395347e690179b
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-21 20:18:22 +0200

    Adds possibility to pass url without --url flag

--- cli.js
+++ cli.js
@@ -13,14 +13,14 @@ function printHelp() {
   console.log(pkg.description);
   console.log('');
   console.log('Usage:');
-  console.log('  $ html-validate --url=<url>');
+  console.log('  $ html-validate <url>');
   console.log('');
   console.log('Or:');
   console.log('  $ html-validator --file=<file>');
   console.log('');
   console.log('Optional, specify format of returned data');
   console.log('Valid options: json, html, xhtml, xml, gnu and text (default)');
-  console.log('  $ html-validate --url=<url> --format=<format>');
+  console.log('  $ html-validate <url> --format=<format>');
 }
 
 if (!query || process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
@@ -31,6 +31,10 @@ if (!query || process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help'
 if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -1) {
   console.log(pkg.version);
   return;
+}
+
+if (query.indexOf('http') !== -1) {
+  opts.url = argv._[0];
 }
 
 if(argv.format){
@@ -46,6 +50,8 @@ if(argv.file){
 }
 
 validator(opts, function(err, data){
-  if(err) throw err;
+  if(err) {
+    throw err;
+  };
   console.log(data);
 });
