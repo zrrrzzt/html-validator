aeb158fee9f7c5f9af44b3adc3a2bd75bd7e9f81
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-09-18 09:49:36 +0200

    Removes tests for missing format

--- test/testInputs.js
+++ test/testInputs.js
@@ -6,25 +6,6 @@ var assert = require('assert')
 
 describe('Validator - inputs', function(){
 
-  it('Should throw if opts.format is not specified', function(done){
-
-    var opts = {url:'http://www.npmjs.org'};
-
-    validator(opts, function(err, data){
-      assert.throws(function(){
-          if(err) throw err;
-        }, function(err){
-          if((err instanceof Error) && /Missing required param: format/.test(err)){
-            return true
-          }
-        },
-        "Unexpected error"
-      );
-      done();
-    });
-
-  });
-
   it('Should throw if opts.url or opts.data is not specified', function(done){
 
     var opts = {format:'json'};
@@ -46,7 +27,7 @@ describe('Validator - inputs', function(){
 
   it('Should throw if opts.url contains invalid url', function(done){
 
-    var opts = {format:'json', url:'pysje'};
+    var opts = {url:'pysje'};
 
     validator(opts, function(err, data){
       assert.throws(function(){
