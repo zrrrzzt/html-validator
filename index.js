29b779a7eaadb0191c5fd01dbe50876653f5f433
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-06-30 20:26:14 +0200

    Solves #60 Promise rejections should return to end execution

--- index.js
+++ index.js
@@ -9,26 +9,17 @@ module.exports = (options, callback) => {
   return new Promise((resolve, reject) => {
     if (!options) {
       let error = new Error('Missing required input: options object')
-      if (callback) {
-        return callback(error, null)
-      }
-      reject(error)
+      return callback ? callback(error, null) : reject(error)
     }
 
     if (!options.url && !options.data) {
       let error = new Error('Missing required params: url or data')
-      if (callback) {
-        return callback(error, null)
-      }
-      reject(error)
+      return callback ? callback(error, null) : reject(error)
     }
 
     if (options.url && !validUrl.isWebUri(options.url)) {
       let error = new Error('Invalid url')
-      if (callback) {
-        return callback(error, null)
-      }
-      reject(error)
+      return callback ? callback(error, null) : reject(error)
     }
 
     const reqOpts = setupOptions(options)
@@ -36,30 +27,21 @@ module.exports = (options, callback) => {
 
     request(reqOpts, (error, response, result) => {
       if (error) {
-        if (callback) {
-          return callback(error, null)
-        }
-        reject(error)
+        return callback ? callback(error, null) : reject(error)
       }
 
       if (response && response.statusCode !== 200) {
         let error = new Error('Validator returned unexpected statuscode: ' + response.statusCode)
-        if (callback) {
-          return callback(error, null)
-        }
-        reject(error)
+        return callback ? callback(error, null) : reject(error)
       }
 
-      var data = options.format === 'json' && !ignore ? JSON.parse(result) : result
+      let data = options.format === 'json' && !ignore ? JSON.parse(result) : result
 
       if (ignore) {
         data = filterData(data, ignore)
       }
 
-      if (callback) {
-        return callback(null, data)
-      }
-      resolve(data)
+      return callback ? callback(null, data) : resolve(data)
     })
   })
 }
