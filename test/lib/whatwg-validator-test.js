952a3fffbf439ccbec617a7eb428e0a6535ad45d
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-02-15 22:28:15 +0100

    Adds test for whatwg validator (patch)

---
+++ test/lib/whatwg-validator-test.js
@@ -0,0 +1,34 @@
+const tap = require('tap')
+const { readFile } = require('fs').promises
+const validator = require('../../lib/whatwg-validator.js')
+
+tap.test('returns valid for valid html', async test => {
+  const html = await readFile('test/data/valid.html', 'utf-8')
+  const options = {
+    data: html
+  }
+  const result = await validator(options)
+  tap.equal(result.isValid, true, 'Valid html ok')
+  return test.done()
+})
+
+tap.test('returns invalid for invalid html', async test => {
+  const html = await readFile('test/data/invalid.html', 'utf-8')
+  const options = {
+    data: html
+  }
+  const result = await validator(options)
+  tap.equal(result.isValid, false, 'invalid html ok')
+  return test.done()
+})
+
+tap.test('it supports ignore', async test => {
+  const html = await readFile('test/data/invalid.html', 'utf-8')
+  const options = {
+    data: html,
+    ignore: ['close-order']
+  }
+  const result = await validator(options)
+  tap.equal(result.isValid, true, 'ignore ok')
+  return test.done()
+})
