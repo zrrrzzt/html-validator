33eb3d222dffe24efe1a0158a5cd6a295492c336
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-02-15 22:42:46 +0100

    Adds validator-test (patch)

---
+++ test/lib/validator-test.js
@@ -0,0 +1,21 @@
+const tap = require('tap')
+const { readFile } = require('fs').promises
+const validator = require('../../index')
+const whatwgResponse = {
+  isValid: true,
+  errorCount: 0,
+  warningCount: 0,
+  errors: [],
+  warnings: []
+}
+
+tap.test('returns whatwg if validator is set', async test => {
+  const html = await readFile('test/data/valid.html')
+  const options = {
+    data: html,
+    validator: 'whatwg'
+  }
+  const results = await validator(options)
+  tap.equal(results, whatwgResponse, 'Got whatwg data')
+  return test.done()
+})
