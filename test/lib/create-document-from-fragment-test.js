748bee7feb9f79ba8727d9f973b2b4af8a4cb4f0
Quentin <quentinmousset@gmail.com>
2024-05-28 13:58:32 +0200

    Implement multi language in fragments

--- test/lib/create-document-from-fragment-test.js
+++ test/lib/create-document-from-fragment-test.js
@@ -3,7 +3,7 @@ const createDocument = require('../../lib/create-document-from-fragment')
 
 tap.test('returns document from fragment', test => {
   const fragment = '<p>This is a fragment</p>'
-  const document = createDocument(fragment)
+  const document = createDocument(fragment, 'en')
   const expectedDocument = '<!DOCTYPE html><html lang="en"><head><title>Document from fragment</title></head><body><p>This is a fragment</p></body></html>'
   tap.equal(document, expectedDocument, 'document is correct')
   test.end()
